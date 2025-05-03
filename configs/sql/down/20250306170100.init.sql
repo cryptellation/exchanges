333e152d1f04ddebecd864801511a00b3899d3fa
Louis FRADIN <louis.fradin@gmail.com>
2025-05-03 14:33:37 +0200

    feat(clients): add workflow client

--- configs/sql/down/20250306170100.init.sql
+++ configs/sql/down/20250306170100.init.sql
@@ -1,5 +1,1 @@
-DROP TABLE candlesticks;
 DROP TABLE exchanges;
-DROP TABLE backtests;
-DROP TABLE forwardtests;
-DROP TABLE indicators_sma;
---
+++ pkg/clients/wfclient.go
@@ -0,0 +1,42 @@
+package clients
+
+import (
+	"github.com/cryptellation/exchanges/api"
+	"go.temporal.io/sdk/workflow"
+)
+
+// Wf Client is a client for the cryptellation exchanges service from a workflow perspective.
+type WfClient interface {
+	// GetExchange calls the exchange get workflow.
+	GetExchange(
+		ctx workflow.Context,
+		params api.GetExchangeWorkflowParams,
+		childWorkflowOptions *workflow.ChildWorkflowOptions,
+	) (result api.GetExchangeWorkflowResults, err error)
+}
+
+type wfClient struct{}
+
+// NewWfClient creates a new workflow client.
+// This client is used to call workflows from within other workflows.
+// It is not used to call workflows from outside the workflow environment.
+func NewWfClient() WfClient {
+	return wfClient{}
+}
+
+// GetExchange gets exchange info from Cryptellation service.
+func (wfClient) GetExchange(
+	ctx workflow.Context,
+	params api.GetExchangeWorkflowParams,
+	childWorkflowOptions *workflow.ChildWorkflowOptions,
+) (result api.GetExchangeWorkflowResults, err error) {
+	// Set options
+	if childWorkflowOptions == nil {
+		childWorkflowOptions = &workflow.ChildWorkflowOptions{}
+	}
+	ctx = workflow.WithChildOptions(ctx, *childWorkflowOptions)
+
+	// Get exchange info
+	err = workflow.ExecuteChildWorkflow(ctx, api.GetExchangeWorkflowName, params).Get(ctx, &result)
+	return result, err
+}
