8e4726be259a5f6fe12e56813bce4933faaa6b01
Louis FRADIN <louis.fradin@gmail.com>
2025-06-26 14:11:15 +0200

    chores: add DB credentials cleanup functionality

--- configs/default.go
+++ configs/default.go
@@ -3,9 +3,9 @@ package configs
 const (
 	// DefaultDBDSN is the default database DSN.
 	DefaultDBDSN = "host=localhost " +
-		"user=cryptellation_exchanges " +
-		"password=cryptellation_exchanges " +
-		"dbname=cryptellation_exchanges " +
+		"user=cryptellation " +
+		"password=cryptellation " +
+		"dbname=exchanges " +
 		"sslmode=disable"
 
 	// DefaultBinanceAPIKey is the default Binance API key.
